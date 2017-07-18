dd83f8ddcf1f21071fec2a859dc5252cd2f0c474
Renato Fialho <renato@renatofialho.com>
2017-07-18 20:59:02 -0300

    Configuration changes

--- config/dev.py
+++ config/dev.py
@@ -2,4 +2,5 @@ class DevConfig(object):
     DEBUG = True
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://renato:1234@localhost:5432/flaskdb'
-    SQLALCHEMY_TRACK_MODIFICATIONS = 'true'
+    SQLALCHEMY_TRACK_MODIFICATIONS =  True
+    SQLALCHEMY_ECHO = True
