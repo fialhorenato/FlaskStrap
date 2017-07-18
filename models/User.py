8f9f64de534b88c98647f1717437279b26621b93
Renato Fialho <renato@renatofialho.com>
2017-07-18 20:58:38 -0300

    Upgrading models

--- models/User.py
+++ models/User.py
@@ -1,14 +1,29 @@
 from models.Shared import db
+from flask import jsonify
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    username = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(120), index=True)
     pass
 
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
     def __init__(self, username, email):
         self.username = username
         self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % (self.username)
+    
+    def _toJson_(self):
+        response = jsonify({
+                    'id': self.id,
+                    'username':self.username,
+                    'email': self.email,
+        })
+        return response
